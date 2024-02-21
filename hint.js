883bce980545906a3ab25d4dd27443e3f2e0dfcb
Tyler Glisson <Tyler@tglisson.com>
2024-02-21 12:54:05 -0500

    scaffold function and tests

---
+++ hint.js
@@ -0,0 +1,10 @@
+function translateInput(input) {
+ 
+    
+}
+
+translateInput('hello')
+translateInput('hello world')
+translateInput('that strange dragon scared them')
+translateInput('an aweful attitude is often unpleasant')
+// more tests after we get to this point for edge cases, uppercase, etc..
