5711f4ef237f96ac4226085f7d99bd842e50bd61
Tyler Glisson <Tyler@tglisson.com>
2024-02-21 13:13:30 -0500

    add notes and setup for uppercase

--- hint.js
+++ hint.js
@@ -9,11 +9,13 @@ function translateLongInput(input) {
 function translateWord(input) {
     // console.log('Input:', input)
     //consider regex??
+    // edge cases with y as a vowel?
     const vowels = ['a', 'e', 'i', 'o', 'u']
     const suffix = 'ay'
     let root = ''
     let prefix = ''
     let result = ''
+    const upper = /^[A-Z]/
 
     if (vowels.includes(input[0])) {
         if (input.length === 1) result = input + 'yay'
@@ -37,4 +39,6 @@ function translateWord(input) {
 // translateLongInput('hello world')
 // translateLongInput('that strange dragon scared them')
 // translateLongInput('an aweful attitude is often unpleasant')
-translateLongInput('a erratta irate')
+// translateLongInput('a erratta irate')
+translateLongInput('Wow! hi.')
+translateLongInput('Such a nice, large, friendly, dragon!')
